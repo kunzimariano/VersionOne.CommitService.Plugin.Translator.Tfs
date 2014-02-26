90003a2e6cbbeba21cd44bc8ae7da08fb8d4a44e
kunzimariano <kunzi.mariano@gmail.com>
2014-02-26 15:50:27 -0300

    Initial commit.

---
+++ TfsTranslator.Tests/TfsTranslatorTests.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using VersionOne.CommitService.Interfaces;
+using VersionOne.CommitService.Types;
+
+namespace VersionOne.CommitService.Plugin.Translator.Tfs.Tests
+{
+	[TestFixture]
+	public class TfsTranslatorTests
+	{
+		private readonly ITranslateInboundMessageToCommits _translator = new TfsTranslator();
+		private InboundMessage _validMessage;
+		private InboundMessage _invalidMessage;
+
+		[SetUp]
+		public void SetUp()
+		{
+			var headers = new Dictionary<string, string[]>()
+			{
+				{ "User-Agent", new[] { "Team Foundation (TfsJobAgent.exe, 10.0.40219.1)" } }
+			};
+
+			var validSampleData = File.ReadAllText(@".\TestData\ValidMessage.xml");
+			_validMessage = new InboundMessage(validSampleData, headers);
+
+
+			var inValidSampleData = File.ReadAllText(@".\TestData\InValidMessage.xml");
+			_invalidMessage = new InboundMessage(inValidSampleData, headers);
+		}
+
+		[Test]
+		public void CanProcess_is_true_for_valid_message_and_useragent()
+		{
+			bool canProcess = _translator.CanProcess(_validMessage);
+			Assert.IsTrue(canProcess);
+		}
+
+		[Test]
+		public void CanProcess_is_false_for_invalid_useragent()
+		{
+			_validMessage.Headers["User-Agent"][0] = "nonsense";
+			bool canProcess = _translator.CanProcess(_validMessage);
+			Assert.IsFalse(canProcess);
+		}
+
+		[Test]
+		public void CanProcess_is_false_for_invalid_body_message()
+		{
+			bool canProcess = _translator.CanProcess(_invalidMessage);
+			Assert.IsFalse(canProcess);
+		}
+
+		[Test]
+		public void Execute_succeeds_for_valid_message()
+		{
+			Translation.Result result = _translator.Execute(_validMessage);
+			Assert.IsTrue(result.TranslationResult.IsRecognized);
+			var translationResult = (InboundMessageResponse.TranslationResult.Recognized)result.TranslationResult;
+			Assert.AreEqual(1, translationResult.commits.Count());
+		}
+
+		[Test]
+		[UseReporter(typeof(DiffReporter))]
+		public void On_Execute_succeeds_commit_result_matches_expectation()
+		{
+			Translation.Result result = _translator.Execute(_validMessage);
+			var translationResult = (InboundMessageResponse.TranslationResult.Recognized)result.TranslationResult;
+			var cm = translationResult.commits.FirstOrDefault();
+			Approvals.Verify(JsonConvert.SerializeObject(cm, Formatting.Indented));
+		}
+
+		[Test]
+		[UseReporter(typeof(DiffReporter))]
+		public void On_Execute_succeeds_response_matches_expectation()
+		{
+			Translation.Result result = _translator.Execute(_validMessage);
+			var content = (InboundMessageResponse.Content.Custom)result.Content;
+			var stringResponse = content.Item.ReadAsStringAsync().Result;
+			Approvals.Verify(stringResponse);
+		}
+
+		[Test]
+		public void Execute_fails_on_non_parsable_input()
+		{
+			Translation.Result result = _translator.Execute(_invalidMessage);
+			Assert.IsTrue(result.TranslationResult.IsFailure);
+		}
+
+		[Test]
+		[UseReporter(typeof(DiffReporter))]
+		public void On_Execute_fails_response_matches_expectation()
+		{
+			Translation.Result result = _translator.Execute(_invalidMessage);
+			var content = (InboundMessageResponse.Content.Custom)result.Content;
+			var stringResponse = content.Item.ReadAsStringAsync().Result;
+			Approvals.Verify(stringResponse);
+		}
+	}
+}
---
+++ TfsTranslator/TfsTranslator.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Xml.Linq;
+using VersionOne.CommitService.Interfaces;
+using VersionOne.CommitService.Types;
+
+namespace VersionOne.CommitService.Plugin.Translator.Tfs
+{
+	[Export(typeof(ITranslateInboundMessageToCommits))]
+	public class TfsTranslator : ITranslateInboundMessageToCommits
+	{
+		private readonly Regex _eventPattern = new Regex(@"(?<=<eventXml>).*?(?=<\/eventXml>)");
+		private readonly Regex _checkInEventPattern = new Regex("<CheckinEvent xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">");
+
+		public bool CanProcess(InboundMessage message)
+		{
+			return IsUserAgentFromTfs(message.Headers) && IsCheckInEvent(message.Body);
+		}
+
+		public Translation.Result Execute(InboundMessage message)
+		{
+			try
+			{
+				XDocument document = DecodeContentAndCreateDocument(message);
+				CommitMessage commit = ParseXmlDocument(document);
+
+				return Translation.SuccessWithContent(
+					new List<CommitMessage> { commit },
+					new StringContent(GetResponseBody(), Encoding.UTF8, GetMediaType())
+					);
+			}
+			catch
+			{
+				return Translation.FailureWithContent(
+					"It was not possible to translate the message.",
+					new StringContent(GetResponseBody(), Encoding.UTF8, GetMediaType())
+					);
+			}
+		}
+
+		private XDocument DecodeContentAndCreateDocument(InboundMessage message)
+		{
+			string content = HttpUtility.HtmlDecode(message.Body);
+			Match match = _eventPattern.Match(content);
+			var document = XDocument.Parse(match.Value);
+			return document;
+		}
+
+		private CommitMessage ParseXmlDocument(XDocument document)
+		{
+			var c = document.Descendants("CheckinEvent").FirstOrDefault();
+
+			return new CommitMessage(
+					new Author() { Name = c.Element("Committer").Value },
+					ParseDateTimeOffset(c),
+					c.Element("Comment").Value,
+					new Repo() { Name = c.Element("TeamProject").Value },
+					"TFS",
+					new CommitId() { Name = c.Element("Number").Value },
+					null);
+		}
+
+		private string GetMediaType()
+		{
+			return "application/soap+xml";
+
+		}
+
+		private string GetResponseBody()
+		{
+			return
+				"<?xml version=\"1.0\"?><s:Envelope xmlns:a=\"http://www.w3.org/2005/08/addressing\" xmlns:s=\"http://www.w3.org/2003/05/soap-envelope\"><s:Header><a:Action s:mustUnderstand=\"1\">http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03/IService/NotifyResponse</a:Action></s:Header><s:Body xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><NotifyResponse xmlns=\"http://schemas.microsoft.com/TeamFoundation/2005/06/Services/Notification/03\"/></s:Body></s:Envelope>";
+		}
+
+		private bool IsUserAgentFromTfs(IDictionary<string, string[]> headers)
+		{
+			return headers.ContainsKey("User-Agent") &&
+				   headers["User-Agent"]
+				   .Any(h => h.StartsWith("Team Foundation"));
+		}
+
+		private bool IsCheckInEvent(string body)
+		{
+			if (string.IsNullOrWhiteSpace(body))
+				return false;
+
+			string decodedBody = HttpUtility.HtmlDecode(body);
+			return _checkInEventPattern.IsMatch(decodedBody);
+		}
+
+		private DateTimeOffset ParseDateTimeOffset(XElement e)
+		{
+			var creationDate = e.Element("CreationDate").Value;
+			// ignores the three last characters since .Net does not parse it (-05:00:00)
+			var offset = e.Element("TimeZoneOffset").Value.Remove(6);
+			string validString = string.Format("{0} {1}", creationDate, offset);
+			return DateTimeOffset.Parse(validString);
+		}
+	}
+}
